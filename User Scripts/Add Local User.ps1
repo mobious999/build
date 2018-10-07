6f288a03d73ce38f3c807607dc710c65814941cd
Mark Quinn <mobious_99@yahoo.com>
2018-10-07 12:34:43 -0400

    Updated examples

--- User Scripts/Add Local User.ps1
+++ User Scripts/Add Local User.ps1
@@ -21,11 +21,21 @@
     $logfolder
 .INPUTS
     See the example below
+    $name
+    $password
+    $Accountexpires
+    $Accountneverexpires
+    $confirm
+    $description
+    $disabled
+    $fullname
+    $passwordneverexpires
+    $usermaynotchangepassword
     $errorlog - the log that gets created on a trapped error
     $logfile - the log of the action and completion
     $logfolder - where the logs get created
 .OUTPUTS
-    
+    Loggiing where needed
 .NOTES
   Version:        1.0
   Author:         Mark Quinn
@@ -34,6 +44,10 @@
   Based on this article
   https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.localaccounts/new-localuser?view=powershell-5.1
 .EXAMPLE
+  Create a user account
+  New-LocalUser -Name "User02" -Description "Description of this account." -NoPassword
+  Create a user account that has a password
+  New-LocalUser "User03" -Password $Password -FullName "Third User" -Description "Description of this account."
   .\add local user -name (name) -password (password) -Accountexpires (date) -confirm $false -description (description of the user) -disabled (true / false) -fullname "name of user" -passwordneverexpires (true/false) -$UserMayNotChangePassword (true/false) -errorlog (logfilename) -logfile (logfilename) -logfolder (path to the log files)
 #>
 
@@ -41,25 +55,25 @@ Param(
   [Parameter(Mandatory=$True,Position=1)]
   [string]$name,
 	
-  [Parameter(Mandatory=$True)]
+  [Parameter(Mandatory=$False)]
   [Securestring]$Password,
 
-  [Parameter(Mandatory=$True)]
+  [Parameter(Mandatory=$False)]
   [string]$AccountExpires,
 
   [Parameter(Mandatory=$False)]
   [string]$AccountNeverExpires,
 
-  [Parameter(Mandatory=$True)]
+  [Parameter(Mandatory=$False)]
   [string]$Confirm,
 
-  [Parameter(Mandatory=$True)]
+  [Parameter(Mandatory=$False)]
   [string]$Description,
 
   [Parameter(Mandatory=$False)]
   [string]$Disabled,
 
-  [Parameter(Mandatory=$True)]
+  [Parameter(Mandatory=$False)]
   [string]$FullName,
 
   [Parameter(Mandatory=$False)]
